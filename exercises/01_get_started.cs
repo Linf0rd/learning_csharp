2768ac79673be38e6cb8f2140d8e4c5ebbe85d20
Linf0rd <linfordsnagger@gmx.com>
2026-06-17 11:28:20 +0200

    Add initial C# project structure with .gitignore and first lesson's program.

---
+++ exercises/01_get_started.cs
@@ -0,0 +1,14 @@
+// Writing my first C# Program
+
+using System;
+
+namespace exercises
+{
+	class GetStarted
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
